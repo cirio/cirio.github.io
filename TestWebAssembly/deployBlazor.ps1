fcd68edb52ec2c28b0f81b94539994f3b932c759
Andrea Cirioni <a.cirioni@hunext.com>
2019-12-31 15:23:02 +0100

    new dist2

---
+++ TestWebAssembly/deployBlazor.ps1
@@ -0,0 +1,14 @@
+$pathToSolution = "C:\Users\a.cirioni\sources\BlazorWebAssembly\BlazorApp1\"
+$projectName = "BlazorApp1"
+$repoName = "BlazorWebAssembly"
+
+Write-Output "----==== Publish $pathToSolution"
+dotnet publish $pathToSolution -c Release -o ./dist/
+Write-Output ""
+
+Write-Output "----==== Copy from ./dist/$projectName/dist"
+Copy-Item -Path "./dist/$projectName/dist/*" -Destination "./" -Recurse -Force
+Write-Output ""
+
+Write-Output "----==== Delete dist folder"
+Remove-Item ./dist/ -Recurse
